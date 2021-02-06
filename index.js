6aa791489aa3aa6ef5df1b57fa12486841658b01
WalterDantas <waltersantosd@gmail.com>
2021-02-05 23:32:41 -0300

    Site 90%

---
+++ index.js
@@ -0,0 +1,21 @@
+const mainBanner = document.getElementById("mainBanner");
+
+const imgs = ['living-room-2569325_1920.jpg','dining-room-3108037.jpg','bed-4416515_1920.jpg'];
+let index = 0;
+
+
+
+
+function changeImg(element) {
+  element.src = `assets/${imgs[index]}`;
+  index++;
+  if(index > 2) {
+    index = 0;
+  }
+}
+
+
+
+setInterval(() => {
+  changeImg(mainBanner);
+}, 4000);
