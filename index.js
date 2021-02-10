cd28eaab7019ef616962c6a0c2749c9c48660592
RaphaelBRodrigues <raphaelbarbosa.rodrigues@gmail.com>
2021-02-10 20:32:17 -0300

    fix(top): adjusting main banner

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const imgs = ['living-room-2569325_1920.jpg','dining-room-3108037.jpg','bed-4416
 let index = 0;
 
 function changeImg(element) {
-  console.info(element);
   element.style.backgroundImage = `url('assets/${imgs[index]}')`;
-  console.log("caraio",element.style);
-  console.info("img",element.style.backgroundImage);
   index++;
   if(index > 2) {
     index = 0;
